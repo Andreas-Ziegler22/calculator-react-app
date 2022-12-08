d90d0f8d11015792693ac3eea7df24bd8a670290
MrBurp <andreass.ziegler@gmail.com>
2022-12-08 13:04:38 +0100

    create buttons

---
+++ src/components/Button.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "./Buttons.css";
+
+export default (props) => <button className="btn">0</button>;
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import "./Calculator.css";
+import Button from "../components/Button";
 
 export default class Calculator extends Component {
   render() {
-    return <div className="calculator"></div>;
+    return (
+      <div className="calculator">
+        <Button />
+      </div>
+    );
   }
 }
