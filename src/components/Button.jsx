ccf02d400f38d21a5ce3a74457ee7323f9c8400e
MrBurp <andreass.ziegler@gmail.com>
2022-12-09 10:35:11 +0100

    button class style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,18 @@
 import React from "react";
 import "./Buttons.css";
 
-export default (props) => <button className="btn">{props.label}</button>;
+export default (props) => {
+  let classes = "btn";
+  classes += props.operation ? "operation" : "";
+  classes += props.double ? "double" : "";
+  classes += props.triple ? "triple" : "";
+
+  return (
+    <button
+      onclick={(e) => props.click && props.click(props.label)}
+      className={classes}
+    >
+      {props.label}
+    </button>
+  );
+};
